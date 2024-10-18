c56efc599f6933535426a175b6f992cf688399da
Sepideh Hayati <126966991+SepidehHayati@users.noreply.github.com>
2024-10-18 10:22:51 +0200

    Update 1_Join.sql

--- Intermediate/1_Join.sql
+++ Intermediate/1_Join.sql
@@ -1,22 +1,20 @@
 #Data Analyst Bootcamp with Alex Freberg
   #Video 09 on YouTube Channel
-  #Beginner MySQL Series
+  #Intermediate MySQL Series
   #Lesson: Join in MySQL
   
 -- Performing an INNER JOIN between employee_demographics and employee_salary based on employee_id
 SELECT *
 FROM employee_demographics 
 INNER JOIN employee_salary 
-	ON employee_demographics.employee_id = employee_salary.employee_id
-;
+	ON employee_demographics.employee_id = employee_salary.employee_id;
 
 
 -- Same INNER JOIN query, but using table aliases (dem for employee_demographics and sa for employee_salary)
 SELECT *
 FROM employee_demographics AS dem
 INNER JOIN employee_salary AS sa
-	ON dem.employee_id = sa.employee_id
-;
+	ON dem.employee_id = sa.employee_id;
 
 
 -- Example of an error due to ambiguous columns (employee_id column not specified with table alias)
@@ -24,24 +22,21 @@ INNER JOIN employee_salary AS sa
 SELECT employee_id, age, occupation
 FROM employee_demographics AS dem
 INNER JOIN employee_salary AS sa
-	ON dem.employee_id = sa.employee_id
-;
+	ON dem.employee_id = sa.employee_id;
 
 
 -- Correcting the previous query by using the dem alias to specify employee_id source
 SELECT dem.employee_id, age, occupation
 FROM employee_demographics AS dem
 INNER JOIN employee_salary AS sa
-	ON dem.employee_id = sa.employee_id
-;
+	ON dem.employee_id = sa.employee_id;
 
 -- Performing a LEFT JOIN to include all records from employee_demographics, 
 -- even if they don't have a match in employee_salary
 SELECT *
 FROM employee_demographics AS dem
 LEFT JOIN employee_salary AS sa
-	ON dem.employee_id = sa.employee_id
-;
+	ON dem.employee_id = sa.employee_id;
 
 
 -- Performing a RIGHT JOIN to include all records from employee_salary, 
@@ -49,8 +44,7 @@ LEFT JOIN employee_salary AS sa
 SELECT *
 FROM employee_demographics AS dem
 RIGHT JOIN employee_salary AS sa
-	ON dem.employee_id = sa.employee_id
-;
+	ON dem.employee_id = sa.employee_id;
 
 
 -- SELF JOIN on employee_salary to find records where one employee_id is exactly one more than another
@@ -81,10 +75,4 @@ FROM employee_demographics AS dem
 INNER JOIN employee_salary AS sal
 	ON dem.employee_id = sal.employee_id
 inner join parks_departments pd
-    on sal.dept_id = pd.department_id
-;
-
-
--- Selecting all records from the parks_departments table
-SELECT *
-FROM parks_departments;
+    on sal.dept_id = pd.department_id;
