ed97b45aaaa2076d72de957f97be1a0522c06c5c
Sepideh Hayati <126966991+SepidehHayati@users.noreply.github.com>
2025-04-16 09:59:54 +0200

    Add files via upload

---
+++ Beginner/1_use_dataset.sql
@@ -0,0 +1,6 @@
+-- 02_how_to_select_specific_dataset.sq
+
+-- This script selects the 'sales' database to be used for the current session.
+-- All subsequent queries will be run within this database context.
+
+USE sales;
